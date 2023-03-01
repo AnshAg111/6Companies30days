a353606892b2e82f4dd8b1cc43ecc7d594918579
AnshAg111 <111360649+AnshAg111@users.noreply.github.com>
2023-03-01 20:01:13 +0530

    Time: 74 ms (98.13%), Space: 40.7 MB (11.93%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    vector<int> v;
+public:
+    
+    Solution(vector<int>& w) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        v.push_back(w[0]);
+        for(int i=1; i<w.size(); i++){
+            v.push_back(v.back()+w[i]);
+        }
+    }
+    
+    int pickIndex() {
+        int random=rand()%(v.back());
+        return upper_bound(v.begin(), v.end(), random)-v.begin();
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
