377827a7fdbc5cbc1c1a1aa226e209ccc9ac84b7
AnshAg111 <111360649+AnshAg111@users.noreply.github.com>
2023-03-03 12:42:36 +0530

    Time: 0 ms (100.00%), Space: 6 MB (15.90%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+//         for(int i=2; i<=n-2; i++){
+//             string s;
+//             int num=n;
+//             while(num!=0){
+//                 int rem=num%i;
+//                 num/=i;
+                
+//             }
+//         }
+        return false;
+    }
+};
